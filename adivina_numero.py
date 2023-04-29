55093220fcafc2f4c3a99caa404eaf4f00c8430f
PetroGucci <mauriciojvr0@gmail.com>
2023-04-29 15:00:41 -0400

    le agrege limites al juego

--- adivina_numero.py
+++ adivina_numero.py
@@ -4,6 +4,8 @@ import random
 def run():
 	numero_aleatorio=random.randint(1,100)
 	numero_elegido=int(input("\nelige un numero del 1 al 100: "))
+	while numero_elegido>100 or numero_elegido <=0:
+		numero_elegido=int(input("\nEl numero que escogiste es invalido, recuerda que debe ser entre 1 al 100\nElige otro numero: "))
 	while numero_elegido != numero_aleatorio:
 		if numero_elegido < numero_aleatorio:
 			print('\nBusca un numero mas grande\n')
