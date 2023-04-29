9414eedea058e3e8f6a369e716ce46699ffd8f49
PetroGucci <mauriciojvr0@gmail.com>
2023-04-29 13:52:12 -0400

    aprendi a usar random y mejore el codigo del reto

--- reto_adivinador.py
+++ reto_adivinador.py
@@ -2,16 +2,20 @@
 # agarre la idea de un estudiante que puso un "adivinador" de letras
 # asi que hice uno de adivinar numeros
 
+import random
+# ahora que aprendi a usar el import ramdon mejore un poco el codigo haciendolo mas aleatorio
+
 def run():
+    numero_aleatorio=random.randint(1,10)
     numero=int(input("\nDel 1 al 10 en que numero estoy pensando? tienes 5 intentos \n\nEscoge un numero: "))
     intentos = 5
     while intentos > 0:
-        if numero == 4:
+        if numero == numero_aleatorio:
             print("\nExcelente, adivinaste el numero!! :D")
             break
         elif numero<0 or numero>10:
             numero=int(input("\nEse numero esta fuera del rango permitido, elige otro por favor: "))
-        elif numero != 4:
+        elif numero != numero_aleatorio:
             intentos=intentos-1
             print("\nlo siento, ese no es el numero correcto, te quedan "+str(intentos)+" intentos\n")
             numero=int(input("Escoge otro numero: "))
